8f3602a399d0f80b6c99a1f0f388c9d22a1df9e1
Krisztián Ádám <adamkrisz@gmail.com>
2016-03-06 18:31:32 +0100

    keyboard hide fix

--- src/TelegramBot/types/reply_keyboard_hide.cr
+++ src/TelegramBot/types/reply_keyboard_hide.cr
@@ -2,14 +2,13 @@ require "json"
 
 module TelegramBot
   class ReplyKeyboardHide
-    # TODO toJSON
     JSON.mapping({
       hide_keyboard: Bool, # TODO must be true
       selective:     {type: Bool, nilable: true},
     })
-  end
 
-  def initialize
-    @hide_keyboard = true
+    def initialize(@selective = nil)
+      @hide_keyboard = true
+    end
   end
 end
