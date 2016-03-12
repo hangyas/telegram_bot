84302a9fec8993bf5eb3c9828e9d55b159b4f967
Krisztián Ádám <adamkrisz@gmail.com>
2016-03-12 19:36:33 +0100

    exception "handling" in polling

--- src/TelegramBot/bot.cr
+++ src/TelegramBot/bot.cr
@@ -7,41 +7,58 @@ require "./http_client_multipart.cr"
 
 module TelegramBot
   abstract class Bot
-    abstract def handle(message : Message)
+    # handle messages
+    # return: true if the message is handled successfully
+    #         this can be useful for overwrited handlers
+    def handle(message : Message) : Bool
+      return false
+    end
 
+    # handle inline query
     def handle(inline_query : InlineQuery)
     end
 
+    # handle choosen inlien query
     def handle(inline_query : ChoosenInlineResult)
     end
 
-    def initialize(@name : String, @token : String, @private_mode = false : Bool, @users = [] of String)
+    # @name name of the bot (not rely used yet)
+    # @token
+    # @private_mode if true the bot will handle request sent by users from @users
+    # @users list of users for private mode
+    def initialize(@name : String, @token : String, @private_mode : Bool = false, @users = [] of String)
       @nextoffset = 0
     end
 
+    # run long polling in a loop
+    # on the current thread!
     def polling
       loop do
-        updates = get_updates
-        updates.each do |u|
-          if msg = u.message
-            if @private_mode && !@users.includes? msg.from!.username!
-              next
+        begin
+          updates = get_updates
+          updates.each do |u|
+            if msg = u.message
+              if @private_mode && !@users.includes? msg.from!.username!
+                next
+              end
+              handle msg
+              if msg.text
+                p msg.text
+              end
+            elsif query = u.inline_query
+              if @private_mode && !@users.includes? query.from.username!
+                next
+              end
+              handle query
+            elsif choosen = u.choosen_inline_result
+              if @private_mode && !@users.includes? choosen.from.username!
+                next
+              end
+              handle choosen
             end
-            handle msg
-            if msg.text
-              p msg.text
-            end
-          elsif query = u.inline_query
-            if @private_mode && !@users.includes? query.from.username!
-              next
-            end
-            handle query
-          elsif choosen = u.choosen_inline_result
-            if @private_mode && !@users.includes? choosen.from.username!
-              next
-            end
-            handle choosen
           end
+        rescue ex
+          pp ex
         end
       end
     end
@@ -82,7 +99,7 @@ module TelegramBot
       request "getMe"
     end
 
-    private def get_updates(offset = @nextoffset, limit = nil : Int32?, timeout = nil : Int32?)
+    private def get_updates(offset = @nextoffset, limit : Int32? = nil, timeout : Int32? = nil)
       data = request "getUpdates", {"offset": "#{offset}"}
 
       r = [] of Update
@@ -104,7 +121,7 @@ module TelegramBot
         }
     end
 
-    def send_message(chat_id : Int32 | String, text : String, parse_mode = nil : String?, disable_web_page_preview = nil : Bool?, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil) : Message
+    def send_message(chat_id : Int32 | String, text : String, parse_mode : String? = nil, disable_web_page_preview : Bool? = nil, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil) : Message
       if reply_markup
         reply_markup = reply_markup.to_json
       end
@@ -123,42 +140,42 @@ module TelegramBot
     end
 
     # photo file or file id
-    def send_photo(chat_id : Int32 | String, photo : ::File | String, caption = nil : String?, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_photo(chat_id : Int32 | String, photo : ::File | String, caption : String? = nil, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendPhoto", chat_id, photo, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_audio(chat_id : Int32 | String, audio : ::File | String, duration = nil : Int32?, performer = nil : String?, title = nil : String?, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_audio(chat_id : Int32 | String, audio : ::File | String, duration : Int32? = nil, performer : String? = nil, title : String? = nil, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendPhoto", chat_id, audio, duration, performer, title, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_document(chat_id : Int32 | String, document : ::File | String, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_document(chat_id : Int32 | String, document : ::File | String, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendDocument", chat_id, document, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_sticker(chat_id : Int32 | String, sticker : ::File | String, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_sticker(chat_id : Int32 | String, sticker : ::File | String, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendSticker", chat_id, sticker, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_video(chat_id : Int32 | String, video : ::File | String, duration = nil : Int32?, caption = nil : String?, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_video(chat_id : Int32 | String, video : ::File | String, duration : Int32? = nil, caption : String? = nil, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendVideo", chat_id, video, duration, caption, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_voice(chat_id : Int32 | String, voice : ::File | String, duration = nil : Int32?, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_voice(chat_id : Int32 | String, voice : ::File | String, duration : Int32? = nil, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendVoice", chat_id, voice, duration, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def send_location(chat_id : Int32 | String, latitude : Float, longitude : Float, reply_to_message_id = nil : Int32?, reply_markup = nil : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil)
+    def send_location(chat_id : Int32 | String, latitude : Float, longitude : Float, reply_to_message_id : Int32? = nil, reply_markup : ReplyKeyboardMarkup | ReplyKeyboardHide | ForceReply | Nil = nil)
       res = def_request "sendLocation", chat_id, latitude, longitude, reply_to_message_id, reply_markup
       Message.from_json res.to_json
     end
 
-    def answer_inline_query(inline_query_id : String, result_array : Array(InlineQueryResult), cache_time = nil : Int32?, is_personal = nil : Bool?, next_offset = nil : String?) : Bool
+    def answer_inline_query(inline_query_id : String, result_array : Array(InlineQueryResult), cache_time : Int32? = nil, is_personal : Bool? = nil, next_offset : String? = nil) : Bool
       # results   Array of InlineQueryResult  Yes   A JSON-serialized array of results for the inline query
       results = "[" + result_array.join(", ") { |a| a.to_json } + "]"
       res = def_request "answerInlineQuery", inline_query_id, cache_time, is_personal, next_offset, results
