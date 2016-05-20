c4bb6767efdea0904276047040a706829c0ca170
Santiago Palladino <spalladino@gmail.com>
2016-05-20 11:30:31 -0300

    Webhook support for Telegram updates
    Add serve method to be used instead of polling
    Add setWebhook method call
    Send requests as answers to the webhook post when possible
    Changes to multipart post to support adding a file from its contents as a string

--- src/TelegramBot/http_client_multipart.cr
+++ src/TelegramBot/http_client_multipart.cr
@@ -1,42 +1,50 @@
 require "http/client"
 
 class HTTP::Client
-  def self.post_multipart(url : String | URI, parts : MultipartBody, headers : HTTP::Headers | Nil = nil) : HTTP::Client::Response
-    headers ||= HTTP::Headers.new
-    headers["Content-Type"] = "multipart/form-data; boundary=#{parts.boundary}"
-    post url, headers, parts.bodyg
+  def self.post_multipart(url : String | URI, parts : MultipartBody | Hash, headers : HTTP::Headers | Nil = nil) : HTTP::Client::Response
+    exec(url) do |client, path|
+      client.post_multipart(path, parts, headers)
+    end
   end
 
-  def self.post_multipart(url : String | URI, params : Hash, headers : HTTP::Headers | Nil = nil) : HTTP::Client::Response
-    parts = HTTP::Client::MultipartBody.new
-    params.each do |k, v|
-      if v.nil?
-        next
-      elsif v.is_a?(::File)
-        parts.add_file(k, v)
-      else
-        parts.add_part(k, v.to_s)
-      end
-    end
-    post_multipart(url, parts, headers)
+  def post_multipart(path, parts : MultipartBody, headers : HTTP::Headers | Nil = nil) : HTTP::Client::Response
+    headers ||= HTTP::Headers.new
+    headers["Content-Type"] = "multipart/form-data; boundary=#{parts.boundary}"
+    post path, headers, parts.bodyg
+  end
+
+  def post_multipart(path, params : Hash, headers : HTTP::Headers | Nil = nil) : HTTP::Client::Response
+    parts = HTTP::Client::MultipartBody.new(params)
+    post_multipart(path, parts, headers)
   end
 
   # ugly represation of multipart/from-data (works for now)
   class MultipartBody
     getter boundary
 
-    @boundary : String
+    @boundary : String = Random.rand(999999).to_s + Random.rand(999999).to_s + Random.rand(999999).to_s + Random.rand(999999).to_s
+    @body : String = ""
 
     def bodyg
       @body + "--" + @boundary + "--\r\n"
     end
 
     def initialize
-      @boundary = Random.rand(999999).to_s + Random.rand(999999).to_s + Random.rand(999999).to_s + Random.rand(999999).to_s
-      @body = ""
     end
 
-    def add_part(name : String, content : String, mime_type : String = "text/plain")
+    def initialize(params : Hash)
+      params.each do |k, v|
+        if v.nil?
+          next
+        elsif v.is_a?(::File)
+          add_file(k, v)
+        else
+          add_part(k, v.to_s)
+        end
+      end
+    end
+
+    def add_part(name : String, content : String, mime_type : String = nil)
       @body += "--" + @boundary + "\r\n"
       @body += "Content-Disposition: form-data; name=\"#{name}\"\r\n"
       if mime_type
@@ -45,20 +53,19 @@ class HTTP::Client
       @body += "\r\n" + content + "\r\n"
     end
 
-    def add_file(name : String, file : ::File, mime_type : String = "application/octet-stream")
+    def add_file(name : String, content : String, filename : String? = nil, mime_type : String = "application/octet-stream")
       @body += "--" + @boundary + "\r\n"
-      @body += "Content-Disposition: form-data; name=\"#{name}\"; filename=\"#{file.path}\"\r\n"
+      @body += "Content-Disposition: form-data; name=\"#{name}\"; filename=\"#{filename}\"\r\n"
       if mime_type
         @body += "Content-Type: #{mime_type}\r\n"
       end
 
-      size = file.size.to_i
-      content = String.new(size) do |buffer|
-        file.read Slice.new(buffer, size)
-        {size.to_i, 0}
-      end
-
       @body += "\r\n" + content + "\r\n"
+    end
+
+    def add_file(name : String, file : ::File | ::Tempfile, filename : String? = nil, mime_type : String = "application/octet-stream")
+      content = File.read(file.path)
+      add_file(name, content, filename || file.path, mime_type)
     end
   end
 end
