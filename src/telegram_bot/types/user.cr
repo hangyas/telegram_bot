8688f2044260d0b4aeef76e0b22b6ef917998379
Krisztián Ádám <adamkrisz@gmail.com>
2017-05-28 20:07:41 +0200

    add language tag

--- src/telegram_bot/types/user.cr
+++ src/telegram_bot/types/user.cr
@@ -3,10 +3,11 @@ require "json"
 module TelegramBot
   class User
     FIELDS = {
-      id:         Int32,
-      first_name: String,
-      last_name:  {type: String, nilable: true},
-      username:   {type: String, nilable: true},
+      id:            Int32,
+      first_name:    String,
+      last_name:     {type: String, nilable: true},
+      username:      {type: String, nilable: true},
+      language_code: {type: String, nilable: true},
     }
     JSON.mapping({{FIELDS}})
     initializer_for({{FIELDS}})
